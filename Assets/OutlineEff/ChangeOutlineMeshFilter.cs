875cb47b35b9e9f00c059919505da84c6d019704
PenakeH <penakeh2549@gmail.com>
2024-11-08 18:00:08 +0700

    add outline

---
+++ Assets/OutlineEff/ChangeOutlineMeshFilter.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class ChangeOutlineMeshFilter : MonoBehaviour
+{
+
+    public GameObject outline;
+    private Mesh meh;
+    private int count = 0;
+    private bool isTerrain = false;
+
+    private void Awake()
+    {
+        if (this.TryGetComponent<Tile>(out Tile tile))
+        {
+            if (this.transform.GetChild(0) != null)
+            {
+                meh = this.transform.GetChild(0).GetComponent<Mesh>();
+            }
+            else
+            {
+                meh = this.transform.GetComponent<Mesh>();
+            }
+            isTerrain = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (outline != null && count == 0 && isTerrain == true)
+        {
+            Instantiate(outline, transform);
+            count++;
+        }
+
+        if (count > 0)
+        {
+            if (this.transform.GetChild(1) != null)
+            {
+                Mesh mesh = this.transform.GetChild(1).GetComponent<Mesh>();
+                mesh = meh;
+            }
+            else
+            {
+                Mesh mesh = this.transform.GetComponent<Mesh>();
+                mesh = meh;
+            }
+        }
+    }
+
+}
