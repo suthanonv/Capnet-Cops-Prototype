f966ecf5b4f047ee9e3ee89ca75927aaffd16e6d
suthanonv <suthanonv@gmail.com>
2024-11-18 11:00:10 +0700

    Import_MonoMap

---
+++ Assets/outlineDesActive.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class outlineDesActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
