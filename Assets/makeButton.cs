3431ed21df6d75bca1b98a5d2e1364217e8f58fc
TheMowo <157827650+TheMowo@users.noreply.github.com>
2024-11-28 21:23:38 +0700

    Fixes to Win & Lose screen (color correction) + Fixed Main Menu

--- Assets/makeButton.cs
+++ Assets/makeButton.cs
@@ -7,9 +7,12 @@ public class makeButton : MonoBehaviour
 {
     public UnityEvent unityEvent;
     public GameObject button;
-
+    private Material defaultMaterial;
+    public Material altMaterial;
+    public Material clickedMaterial;
     private void Start()
     {
+        defaultMaterial = GetComponent<Renderer>().material; //sets this as current material
         button = this.gameObject;
     }
     private void Update()
@@ -17,9 +20,25 @@ public class makeButton : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+        {
+            this.GetComponent<Renderer>().material = altMaterial;
+            Debug.Log("Raycast Hit: Hovering");
+        }
+        else if(this.GetComponent<Renderer>().material != defaultMaterial)
+        {
+            this.GetComponent<Renderer>().material = defaultMaterial;
+            Debug.Log("Raycast Gone: Reset to default.");
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-            unityEvent.Invoke();
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject) 
+            {
+                this.GetComponent<Renderer>().material = clickedMaterial;
+                Debug.Log("Raycast Hit && Clicked: Switched to click material");
+                unityEvent.Invoke(); 
+            }
         }
     }
 }
